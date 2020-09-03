67d88be0718d223869ccf1ea7f2710445e6dbf6b
Gavin Swofford <gavinswofford@gmail.com>
2020-09-03 12:05:56 -0500

    finished smack talking player

--- SmackTalkingPlayer.cs
+++ SmackTalkingPlayer.cs
@@ -1,10 +1,25 @@
+using System;
+using System.Collections.Generic;
 namespace ShootingDice
+
 {
     // TODO: Complete this class
 
     // A Player who shouts a taunt every time they roll dice
     public class SmackTalkingPlayer
     {
-        public string Taunt { get; }
+        private List<string> insultList = new List<string>()
+        {
+            "boo",
+            "you suck",
+            "you're going to loose",
+            "your mama sucks"
+        };
+
+        public void roll()
+        {
+            int randomInsultIndex = new Random().Next(insultList.Count);
+            Console.WriteLine(insultList[randomInsultIndex]);
+        }
     }
 }
