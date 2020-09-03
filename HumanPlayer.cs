0dc2a0fe155090ced6e40b2f4efd87d24ff65d84
Gavin Swofford <gavinswofford@gmail.com>
2020-09-03 14:07:16 -0500

    finsihed human player

--- HumanPlayer.cs
+++ HumanPlayer.cs
@@ -1,10 +1,32 @@
+using System;
+
 namespace ShootingDice
 {
     // TODO: Complete this class
 
     // A player the prompts the user to enter a number for a roll
-    public class HumanPlayer
+    public class HumanPlayer : Player
     {
+        private int rollInt;
+        public override int Roll()
+        {
+            bool stringParse = false;
+            while (!stringParse)
+            {
 
+                Console.Write("What number would you like to roll");
+                string userResponse = Console.ReadLine();
+
+
+                bool userInt = Int32.TryParse(userResponse, out rollInt);
+                if (userInt)
+                {
+                    break;
+                }
+
+            }
+            return rollInt;
+
+        }
     }
 }
